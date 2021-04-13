ff44a08a11ba0f02742d4b0ce638f7be649b0797
AsgeirAlbretsen <bjellanda@gmail.com>
2021-04-14 01:48:47 +0200

    Added Firebase and Firestore

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC9a6_YhqBctHl4Ue6MNb8cfFdpo9ODyPE",
+    authDomain: "tilbudsfinneren.firebaseapp.com",
+    projectId: "tilbudsfinneren",
+    storageBucket: "tilbudsfinneren.appspot.com",
+    messagingSenderId: "941719440548",
+    appId: "1:941719440548:web:a38463f6674fec88186a31",
+    measurementId: "G-8RRGH22B6R"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
+db.collection("week15").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        //console.log(`${doc.id} => ${doc.data()}`);
+        console.log("DATA: " + doc.data().name);
+    });
+});
 
 function animateHamburger(x) { // Activates the css animation for the hamburger menu
     x.classList.toggle("change");
-  }
+}
 
 
 
