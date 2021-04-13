c874d46308ca81df5b00911a3f77692f22025a11
TooDamnFishy <44068928+TooDamnFishy@users.noreply.github.com>
2021-04-14 01:17:35 +0200

    Update

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function closeNav() {
 /*=====================================================================================
 									 Discount list
 =======================================================================================*/
-function createListItem() { // Takes straight from other game of mine, serves just as example
+function createListItem() { // Taken from other game of mine, serves just as example
     var str = document.createElement('section');
     str.innerHTML = '<img draggable="false" onclick="oreFloat()" id="ore' + oreNum + '" class="ore" src="images/' + amounts[0][oresDisplayed] + '.png"/><br /><section id="oreText">+' + amounts[1][oresDisplayed] + ' ' + amounts[0][oresDisplayed] + '!</section>';
     byId("oreAnchor").append(str);
